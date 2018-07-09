a9b87b3c908d0439ad1e67afc5953e1660a01a12
exMachina <shyampadia@live.com>
2018-07-08 23:37:20 -0700

    can defeat hard ai

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,12 @@ from sc2.player import Bot, Computer
 from sc2.constants import UnitTypeId as uti
 
 class StarBot(sc2.BotAI):
+
+    ITERATIONS_PER_MINUTE = 165
+    MAX_WORKERS = 65
+
     async def on_step(self, iteration):
+        self.iteration = iteration
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
@@ -16,9 +21,11 @@ class StarBot(sc2.BotAI):
         await self.attack()
 
     async def build_workers(self):
-        for nexus in self.units(uti.NEXUS).ready.noqueue:
-            if self.can_afford(uti.PROBE):
-                await self.do(nexus.train(uti.PROBE))
+        if self.units(uti.NEXUS).amount*16 > self.units(uti.PROBE).amount:
+            if self.units(uti.PROBE).amount < self.MAX_WORKERS:
+                for nexus in self.units(uti.NEXUS).ready.noqueue:
+                    if self.can_afford(uti.PROBE):
+                        await self.do(nexus.train(uti.PROBE))
 
     async def build_pylons(self):
         if self.supply_left < 5 and not self.already_pending(uti.PYLON):
@@ -43,38 +50,57 @@ class StarBot(sc2.BotAI):
         if self.units(uti.NEXUS).amount < 3 and self.can_afford(uti.NEXUS):
             await self.expand_now()
 
-    # how will this work out with all sites or all nexuses or are they individual to each site?
+    # how will this work out with all sites or all nexuses or are they individual to each site? (thats why its only creating one)
     async def offensive_force_buildings(self):
         if self.units(uti.PYLON).ready.exists:
             pylon = self.units(uti.PYLON).ready.random
             if self.units(uti.GATEWAY).ready.exists and not self.units(uti.CYBERNETICSCORE):
-                # checks if already a cyberneticscore exists  should check if near a nexus
+                # checks if already a cyberneticscore exists should check if near a nexus(should check and build near each nexus and pylon)
                 if self.can_afford(uti.CYBERNETICSCORE) and not self.already_pending(uti.CYBERNETICSCORE):
                     await self.build(uti.CYBERNETICSCORE, near=pylon)
-            elif len(self.units(uti.GATEWAY)) < 3:
+
+            elif self.units(uti.GATEWAY).amount < (self.iteration / self.ITERATIONS_PER_MINUTE / 2):
                 if self.can_afford(uti.GATEWAY) and not self.already_pending(uti.GATEWAY):
                     await self.build(uti.GATEWAY, near=pylon)
 
+            if self.units(uti.CYBERNETICSCORE).ready.exists:
+                if self.units(uti.STARGATE).amount < (self.iteration / self.ITERATIONS_PER_MINUTE / 2):
+                    if self.can_afford(uti.STARGATE) and not self.already_pending(uti.STARGATE):
+                        await self.build(uti.STARGATE, near=pylon)
+
     async def build_offensive_force(self):
         for gw in self.units(uti.GATEWAY).ready.noqueue:
-            if self.can_afford(uti.STALKER) and self.supply_left > 0:
-                await self.do(gw.train(uti.STALKER))
+            if not self.units(uti.STALKER).amount > self.units(uti.VOIDRAY).amount:
+                if self.can_afford(uti.STALKER) and self.supply_left > 0:
+                    await self.do(gw.train(uti.STALKER))
+
+        for sg in self.units(uti.STARGATE).ready.noqueue:
+            if self.can_afford(uti.VOIDRAY) and self.supply_left > 0:
+                await self.do(sg.train(uti.VOIDRAY))
 
     def find_target(self, state):
         if len(self.known_enemy_units) > 0:
             return random.choice(self.known_enemy_units)
-        elif len(self.known_enemy_units) > 0:
+        elif len(self.known_enemy_structures) > 0:
             return random.choice(self.known_enemy_structures)
         else:
             return self.enemy_start_locations[0]
 
     async def attack(self):
-        if self.units(uti.STALKER).amount > 15:
-            for s in self.units(uti.STALKER).idle:
-                await self.do(s.attack(self.find_target(self.state)))
-        elif self.units(uti.STALKER).amount > 3 and len(self.known_enemy_units) > 0:
-            for s in self.units(uti.STALKER).idle:
-                await self.do(s.attack(random.choice(self.known_enemy_units)))
+        # {UNIT: [n to fight, n to defend]}
+        aggressive_units = {
+            uti.STALKER: [15, 3],
+            uti.VOIDRAY: [8, 3]
+        }
+
+        for unit, count in aggressive_units.items():
+            if self.units(unit).amount > count[0]:
+                for s in self.units(unit).idle:
+                    await self.do(s.attack(self.find_target(self.state)))
+            elif self.units(unit).amount > count[1]:
+                if len(self.known_enemy_units) > 0:
+                    for s in self.units(unit).idle:
+                        await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 
 run_game(maps.get("AcidPlantLE"), [Bot(Race.Protoss, StarBot()), Computer(Race.Terran, Difficulty.Hard)], realtime=False)
